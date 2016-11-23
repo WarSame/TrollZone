449128e73694f3e4253ade77edb35ea65f024317
WarSame <graemecliffe@gmail.com>
2016-11-22 23:15:44 -0500

    Redone move method. Cleaned up creature design. Need to stop Troll from being spastic.

--- TrollZone.py
+++ TrollZone.py
@@ -5,25 +5,27 @@ import random
 class Level:
     def __init__(self):
         self.grid = [
-            [Human(0, 0), 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
             [0, 1, 0, 0, 0],
             [0, 1, 0, 1, 1],
             [1, 1, 0, 1, 1],
-            [0, 1, 0, 1, 1],
-            [0, 1, 0, 1, 1],
-            [0, 1, Creature(2, 6), 1, 1],
+            [0, 1, 0, 0, 0],
             [0, 1, 0, 1, 0],
-            [1, 1, 1, 0, Door(4, 8)]
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [1, 1, 1, 0, 0]
         ]
-        self.player = self.grid[0][0]
         self.grid_width = len(self.grid[0])
         self.grid_height = len(self.grid)
         self.done = False
-        self.player_dead = False
+        self.add_creature(Creature(2, 6))
+        self.add_creature(Door(4, 8))
+        self.player = Human(0, 0)
+        self.add_creature(self.player)
 
-    def add_creature(self, creature, x, y):
+    def add_creature(self, creature):
         # Add creature to level list
-        self.grid[y][x] = creature(x, y)
+        self.grid[creature.y][creature.x] = creature
 
     def is_block(self, x, y):
         # Get whether location has a block
@@ -43,7 +45,7 @@ class Level:
         # Remove creature from level list. Notify if player died
         self.grid[creature.y][creature.x] = 0
         if isinstance(creature, Human):
-            self.player_dead = True
+            self.player.is_dead = True
 
     def move_creature(self, creature):
         # Move NPCs in creature list for level
@@ -51,37 +53,31 @@ class Level:
 
     def prowl(self, creature):
         # Move in random directions
-        {
-            0: self.move_down,
-            1: self.move_left,
-            2: self.move_right,
-            3: self.move_up
-        }[random.randint(0, 3)](creature)
+        direction = random.randint(0, 3)
+        if direction == 0:
+            self.move(creature, -1, 0)
+        if direction == 1:
+            self.move(creature, 1, 0)
+        if direction == 2:
+            self.move(creature, 0, -1)
+        if direction == 3:
+            self.move(creature, 0, 1)
 
-    def move_left(self, creature):
-        max_move = self.get_max_distance(creature, -1, 0)
-        creature.x -= max_move
-
-    def move_right(self, creature):
-        max_move = self.get_max_distance(creature, 1, 0)
-        creature.x += max_move
-
-    def move_down(self, creature):
-        max_move = self.get_max_distance(creature, 0, 1)
-        creature.y += max_move
-
-    def move_up(self, creature):
-        max_move = self.get_max_distance(creature, 0, -1)
-        creature.y -= max_move
+    def move(self, creature, direction_x, direction_y):
+        max_move = self.get_max_distance(creature, direction_x, direction_y)
+        creature.x += direction_x * max_move
+        creature.y += direction_y * max_move
 
     def get_max_distance(self, creature, direction_x, direction_y):
         # Maximum distance available to move in a certain direction
         move_dist = 0
-        for i in range(creature.speed):
-            if not self.is_valid_destination(creature.x + (i + 1) * direction_x, creature.y + (i + 1) * direction_y):
-                return move_dist
-            else:
+        for i in range(1, creature.speed + 1):
+            next_x = creature.x + i * direction_x
+            next_y = creature.y + i * direction_y
+            if self.is_valid_destination(next_x, next_y):
                 move_dist += 1
+            else:
+                return move_dist
         return move_dist
 
 
@@ -99,29 +95,29 @@ class Game:
 
     def handle_events(self):
         # Handle event queue
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    self.level.player.flip_color()
-                if event.key == pygame.K_LEFT:
-                    self.level.move_left(self.level.player)
-                if event.key == pygame.K_RIGHT:
-                    self.level.move_right(self.level.player)
-                if event.key == pygame.K_DOWN:
-                    self.level.move_down(self.level.player)
-                if event.key == pygame.K_UP:
-                    self.level.move_up(self.level.player)
-            if event.type == pygame.QUIT:
-                self.game_over = True
+        # Grab first event and then clear queue
+        event = pygame.event.poll()
+        pygame.event.clear()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.level.move(self.level.player, -1, 0)
+            if event.key == pygame.K_RIGHT:
+                self.level.move(self.level.player, 1, 0)
+            if event.key == pygame.K_UP:
+                self.level.move(self.level.player, 0, -1)
+            if event.key == pygame.K_DOWN:
+                self.level.move(self.level.player, 0, 1)
+        if event.type == pygame.QUIT:
+            self.game_over = True
 
-    def draw_creature(self, creature):
+    def draw_map_object(self, map_object):
         # Draw specific creature at specific location
-        pixel_location = self.get_adjusted_pixel_location(creature)
-        self.screen.blit(self.get_character(creature), pixel_location)
+        pixel_location = self.get_adjusted_pixel_location(map_object)
+        self.screen.blit(self.get_character(map_object), pixel_location)
 
-    def get_character(self, creature):
+    def get_character(self, map_object):
         # Return surface for text character
-        return self.font.render(creature.character, False, creature.color)
+        return self.font.render(map_object.character, False, map_object.color)
 
     def get_pixel_location(self, x, y):
         # Get location in pixels of block given x, y co-ords
@@ -140,8 +136,8 @@ class Game:
         padding = 1
         for y, row in enumerate(self.level.grid):
             for x, column in enumerate(row):
-                if isinstance(self.level.grid[y][x], Creature):
-                    self.draw_creature(self.level.grid[y][x])
+                if isinstance(self.level.grid[y][x], MapObject):
+                    self.draw_map_object(self.level.grid[y][x])
                 if self.level.is_block(x, y):
                     rect = pygame.Rect(self.get_pixel_location(x, y),
                                        (self.cell_width - padding, self.cell_height - padding))
@@ -160,51 +156,55 @@ class Game:
         self.screen.fill((0, 0, 0))
         self.draw_blocks()
         pygame.display.flip()
-        self.clock.tick(5)
+        self.clock.tick(60)
 
     def end_game(self):
         # Colour screen and pause movement
-        if self.level.player_dead:
+        if self.level.player.is_dead:
             self.screen.fill((166, 16, 30))
             pygame.display.flip()
             pygame.time.wait(2500)
 
     def run_game(self):
         # Heartbeat
-        while not (self.game_over or self.level.player_dead):
+        while not (self.game_over or self.level.player.is_dead):
             self.handle_events()
             self.move_creatures()
             self.draw_screen()
+            pygame.time.wait(100)
         self.end_game()
 
 
-class Creature:
-    def __init__(self, x, y, color=(34, 139, 34)):
+class MapObject:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.color = color
+        self.color = (255, 255, 255)
         self.size = 1
         self.character = "T"
         self.speed = 1
 
 
+class Creature(MapObject):
+    def __init__(self, x, y, color=(34, 139, 34)):
+        super().__init__(x, y)
+        self.color = color
+
+
 class Human(Creature):
     def __init__(self, x, y):
         super().__init__(x, y, (0, 128, 255))
-        self.is_blue = True
         self.character = "P"
-
-    def flip_color(self):
-        self.is_blue = not self.is_blue
-        if self.is_blue:
-            self.color = (0, 128, 255)
-        else:
-            self.color = (255, 255, 255)
+        self.is_dead = False
 
 
-class Door(Creature):
+class Door(MapObject):
     def __init__(self, x, y):
-        super().__init__(x, y, (150, 75, 0))
+        super().__init__(x, y)
+        self.x = x
+        self.y = y
+        self.color = (150, 75, 0)
+        self.size = 1
         self.speed = 0
         self.character = "D"
 
